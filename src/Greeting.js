f603afbe8d3b12768d626248a782d29777be34e4
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-09-19 12:19:10 +0800

    updated texts

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,10 +1,10 @@
 import React from 'react' 
 function Greeting(props) {
     return(
-       <><h1>{props.name}
-       {props.Age}
-       {props.Description}
-       {props.Location}</h1>
+       <><h1>{props.name}</h1>
+       <h2> {props.Age}</h2>
+       <h3> {props.Description}</h3>
+       <h4>{props.Location}</h4>
        </>
        
     );
